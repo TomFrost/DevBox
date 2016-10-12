297696ded42a8964bb01ef62acd78011723483ef
TomFrost <tom@frosteddesign.com>
2016-10-11 21:06:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# DevBox
+A minimal bootstrapper to turn a brand new Mac into a docker-based dev environment
+
+## Installation
+To get up and running without cloning the repo:
+
+```
+curl https://raw.githubusercontent.com/TechnologyAdvice/devbox/master/install/mac_install.sh > /tmp/mac_install.sh
+sh /tmp/mac_install.sh
+```
+
+## Who is this for?
+DevBox was created to standardize development config at [TechnologyAdvice](http://www.technologyadvice.com), but nothing this script does is company-specific. If you're looking for a great baseline for getting started with Docker-based development on a Mac, DevBox might be a good place to start.
+
+## What does it do?
+After a successful run, the Mac will have an active, super performant Docker environment. A Docker VM is installed into macOS's native hypervisor (xhyve), and the `/Users` folder is mounted on it via a local NFS share -- the fastest measured way to mount Docker volumes on a Mac. The VM is also outfitted with [FS-EventStream](https://github.com/TechnologyAdvice/fs_eventstream) to allow a client on the Mac to forward file change events into the VM. This allows applications like webpack, gulp, nodemon, and any other inotify listener to see when files change on the host. This VM will be autostarted every time the Mac is booted, with the help of a lunch agent installed in `~/Library/LaunchAgents`.
+
+In addition, DevBox installs [DevLab](https://github.com/TechnologyAdvice/DevLab), a docker-compose alternative with built-in FS-EventStream support, which also exposes any container-exposed ports on localhost for the full just-like-Linux experience.
+
+Some other software is installed to support the above. That includes:
+- [Homebrew](http://brew.sh)
+- [Node.js](http://nodejs.org)
+- docker-machine
+- [docker-machine-nfs](https://github.com/adlogix/docker-machine-nfs)
+- tmux (necessary to start xhyve on boot)
+
+Note that all software installs are idempotent. For example, if you've already downloaded and installed the official Node.js package from their website, DevBox won't install it again from brew.
+
+## Why not use the new Docker for Mac?
+[Docker for Mac](https://docs.docker.com/docker-for-mac/) was recently released by the Docker team to automate the process of running Docker inside of xhyve on Mac. At the moment, Docker for Mac does not cover the full scope of development use cases due to various bugs and performance issues, particularly with the [problems in osxfs](https://forums.docker.com/t/file-access-in-mounted-volumes-extremely-slow-cpu-bound/8076) that cause some filesystem-heavy operations to take minutes rather than seconds. DevBox automatically creates a very similar environment, but mounts volumes using the more performant NFS, forwarding fsevents out-of-band.
+
+## How can I configure it?
+DevBox is just a shell script, so feel free to fork it and modify it for your own needs! Out of the box, you can change the docker-machine name by setting the `MACHINE_NAME` environment variable before running the script (default is devbox). Also by default, new files created within the VM/container volumes will be owned by your Mac user. For a native Linux experience, edit /etc/exports to remove the `-mapall=501:20` argument.
+
+## License
+DevBox is ISC licensed. See details in LICENSE.txt.
+
+## Credits
+DevBox was created by Tom Shawver at TechnologyAdvice in 2016.
+
---
+++ install/mac_install.sh
@@ -0,0 +1,378 @@
+#!/bin/sh
+###############################################################
+#
+# Copyright (c) 2016 TechnologyAdvice, LLC
+# See LICENSE.txt for software license details.
+#
+# Portions taken from the fantastic docker-machine-nfs,
+# which is Copyright 2015 Toni Van de Voorde (MIT). Find it
+# here: https://github.com/adlogix/docker-machine-nfs
+#
+###############################################################
+
+if [ -z "$MACHINE_NAME" ]; then
+  MACHINE_NAME=devbox
+fi
+
+EVENTBRIDGE_INSTALL_URL="https://raw.githubusercontent.com/TechnologyAdvice/fs_eventbridge/master/scripts/boot2docker_install.sh"
+FS_SCRIPT_PATH="/tmp/fs_install.sh"
+SHELLCONF_START="## BEGIN DEVBOX CONFIG"
+SHELLCONF_END="## END DEVBOX CONFIG"
+SHELLCONF_TMP="${HOME}/.devbox_tmp"
+
+hello() {
+  cat <<EOF
+This script will configure a Mac running the latest verssion of macOS to run the
+standard TechnologyAdvice development environment. This script strives to be
+idempotent: it will check for the end result before completing an action meant
+to enact that result. This means that while this script may, for example, install
+Node.js from Homebrew, it will not if Node.js has been previously installed by
+any other means.
+
+The actions this script will take:
+- Install Homebrew
+- Install the latest stable Node.js/NPM
+- Globally install DevLab from NPM
+- Install docker and docker-machine
+- Install xhyve and the xhyve driver for docker-machine
+- Create a docker-machine named "$MACHINE_NAME" (change this by setting the
+  MACHINE_NAME env var)
+- Reconfigure the docker VM to mount volumes using NFS (uses docker-machine-nfs)
+- Enable file system event streaming on the docker VM (uses fs_eventbridge)
+
+Root access may be required for some of the above. You will be promped for the
+password during the process if this is necessary.
+
+EOF
+  if [ "$1" != "--noprompt" ]; then
+    read -p "Press Enter to proceed, or CTRL+C to cancel. "
+  fi
+}
+
+echoError() {
+  echo "\033[0;31mFAIL\n\n$1 \033[0m"
+}
+
+echoWarn() {
+  echo "\033[0;33m$1 \033[0m"
+}
+
+echoSuccess() {
+  echo "\033[0;32m$1 \033[0m"
+}
+
+echoInfo() {
+  printf "\033[1;34m[INFO] \033[0m%-62s" "$1"
+}
+
+checkForExec() {
+  echoInfo "Checking for $1 ..."
+
+  if type $1 >/dev/null 2>&1; then
+    echoSuccess "Found"
+    return 0
+  else
+    echoWarn "Not Found"
+    return 1
+  fi
+}
+
+installBrew() {
+  echoInfo "Installing brew ..."
+  echo #EMPTY_LINE
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  if [ "$?" -ne "0" ]; then
+    echoError "Failed to install brew. Please install manually, then run this script again."
+    exit 1
+  fi
+}
+
+installPackage() {
+  echoInfo "Installing $1 via brew ..."
+  echo #EMPTY_LINE
+  brew install $1
+  if [ "$?" -ne "0" ]; then
+    echoError "Failed to install $1. Please correct errors, then run this script again."
+    exit 2
+  else
+    echoSuccess "Successfully installed $1."
+    echo #EMPTY_LINE
+  fi
+}
+
+installNPMPackage() {
+  echoInfo "Installing $1 from NPM ..."
+  npm install -g $1
+  if [ "$?" -ne "0" ]; then
+    echoError "Failed to install $1. Please correct errors, then run this script again."
+    exit 3
+  else
+    echoSuccess "Successfully installed $1."
+    echo #EMPTY_LINE
+  fi
+}
+
+safeInstall() {
+  if ! checkForExec $1; then
+    installPackage $1
+  fi
+}
+
+getRealPath() {
+  FILEPATH=`which $1`
+  FILEDIR=`dirname $FILEPATH`
+  LINKPATH=`readlink $FILEPATH`
+  if [ -n "$LINKPATH" ]; then
+    if [[ "${LINKPATH:0:1}" == "/" ]]; then
+      echo $LINKPATH
+    else
+      echo ${FILEDIR}/${LINKPATH}
+    fi
+  else
+    echo $FILEPATH
+  fi
+}
+
+checkSetuid() {
+  echoInfo "Checking for setuid bit on $1 ..."
+  REALPATH=`getRealPath $1`
+  IS_SUID=`ls -l $REALPATH | grep "^...s"`
+  if [ -n "$IS_SUID" ]; then
+    echoSuccess "Set"
+    return 0
+  else
+    echoWarn "Not Set"
+    return 1
+  fi
+}
+
+setSetuid() {
+  echoInfo "Setting root owner and setuid bit on $1 ..."
+  echo #EMPTY_LINE
+  echoWarn " !!! Sudo will be necessary to set root permissions on $1 !!!"
+  REALPATH=`getRealPath $1`
+  sudo chown root:wheel $REALPATH
+  RES_A=$?
+  sudo chmod u+s $REALPATH
+  RES_B=$?
+  if [ "$RES_A" -eq "0" ] && [ "$RES_B" -eq "0" ]; then
+    echoSuccess "Done"
+    echo #EMPTY_LINE
+  else
+    echoError "Unable to change permissions. Please correct and run this script again."
+    exit 4
+  fi
+}
+
+checkMachineExists() {
+  echoInfo "Checking if $1 machine exists ..."
+  if docker-machine status $1 > /dev/null 2>&1; then
+    echoSuccess "Exists"
+    return 0
+  else
+    echoWarn "Not Found"
+    return 1
+  fi
+}
+
+createMachine() {
+  echoInfo "Creating docker-machine $1 ..."
+  echo #EMPTY_LINE
+  docker-machine create --driver xhyve $1
+  if [ "$?" -eq "0" ]; then
+    echoSuccess "Machine '$1' created successfully"
+  else
+    echoError "Failed creating machine '$1'. Please correct and run this script again."
+    exit 5
+  fi
+}
+
+checkMachineRunning() {
+  echoInfo "Checking that machine $1 is running ..."
+  RUNNING=`docker-machine status $1`
+  if [ "$RUNNING" == "Running" ]; then
+    echoSuccess "Running"
+    return 0
+  else
+    echoWarn "Not Running"
+    return 1
+  fi
+}
+
+startMachine() {
+  echoInfo "Starting machine $1 ..."
+  echo #EMPTY_LINE
+  docker-machine start $1
+  if [ "$?" -eq "0" ]; then
+    echoSuccess "Machine '$1' started successfully"
+  else
+    echoError "Failed starting machine '$1'. Please correct and run this script again."
+    exit 5
+  fi
+}
+
+installNFS() {
+  echoInfo "Running docker-machine-nfs ..."
+  echo #EMPTY_LINE
+  docker-machine-nfs $1
+  if [ "$?" -eq "0" ]; then
+    echoSuccess "docker-machine-nfs has run successfully"
+    echo #EMPTY_LINE
+  else
+    echoError "docker-machine-nfs failed. Please correct and run this script again."
+    exit 6
+  fi
+}
+
+installEventBridge() {
+  echoInfo "Downloading FS-EventBridge installer ..."
+  curl -s $EVENTBRIDGE_INSTALL_URL -o $FS_SCRIPT_PATH > /dev/null 2>&1
+  if [ "$?" -gt "1" ]; then
+    echoError "Failed to download FS-EventBridge installer. Is Github down?"
+    exit 7
+  fi
+  chmod +x $FS_SCRIPT_PATH
+  if [ "$?" -gt "1" ]; then
+    echoError "Failed to amek $FS_SCRIPT_PATH executable. Please correct and run this script again."
+    exit 8
+  fi
+  echoSuccess "Done"
+  echoInfo "Running FS-EventBridge installer on $1 ..."
+  eval $(docker-machine env $1)
+  echo #EMPTY_LINE
+  $FS_SCRIPT_PATH --noprompt
+  if [ "$?" -eq "0" ]; then
+    echoSuccess "FS-Eventbridge has been installed successfully"
+    echo #EMPTY_LINE
+  else
+    echoError "The FS-EventBridge installer has failed. Please correct and run this script again."
+    exit 9
+  fi
+}
+
+writeLaunchAgent() {
+  echoInfo "Writing LaunchAgent ..."
+  AGENT_PATH=${HOME}/Library/LaunchAgents/com.docker.machine.${1}.plist
+  cat << EOF > $AGENT_PATH
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>EnvironmentVariables</key>
+    <dict>
+      <key>PATH</key>
+      <string>${PATH}</string>
+    </dict>
+    <key>Label</key>
+    <string>com.docker.machine.${1}</string>
+    <key>ProgramArguments</key>
+    <array>
+    <string>$(which tmux)</string>
+      <string>-c</string>
+      <string>$(which docker-machine) start ${1}; exit</string>
+    </array>
+    <key>RunAtLoad</key>
+    <true/>
+  </dict>
+</plist>
+EOF
+  launchctl load $AGENT_PATH > /dev/null 2>&1
+  echoSuccess "Done"
+}
+
+checkShell() {
+  if echo $SHELL | grep "/$1\$" > /dev/null 2>&1; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+getShellConfigFile() {
+  if checkShell zsh; then
+    echo "${HOME}/.zshrc"
+  elif checkShell bash; then
+    echo "${HOME}/.profile"
+  fi
+}
+
+getShellConfigLines() {
+  cat <<EOF
+
+$SHELLCONF_START
+eval \$(docker-machine env $1 2> /dev/null)
+export FS_EVENTBRIDGE_PORT=65056
+$SHELLCONF_END
+EOF
+}
+
+userDoShellConfig() {
+  echo #EMPTY_LINE
+  echoWarn "!!! Audience participation required !!!"
+  echoWarn "Please add the following lines to your shell's config file:"
+  getShellConfigLines $1
+  echo #EMPTY_LINE
+  echoWarn "Continuing with the script in 3 seconds. Remember to scroll back here!"
+  echo #EMPTY_LINE
+  sleep 3
+}
+
+writeShellConfig() {
+  echoInfo "Writing shell config ..."
+  FILE=`getShellConfigFile`
+  if [ -z "$FILE" ]; then
+    echoWarn "Cannot determine config file"
+    userDoShellConfig
+  else
+    touch $FILE
+    sed "/^$SHELLCONF_START\$/,/^$SHELLCONF_END\$/d" $FILE > $SHELLCONF_TMP
+    cat << EOF > $FILE
+$(cat $SHELLCONF_TMP)
+$(getShellConfigLines $1)
+
+EOF
+    RES=$?
+    rm -f $SHELLCONF_TMP
+    if [ "$RES" -gt "0" ]; then
+      echoWarn "Failed"
+      userDoShellConfig
+    else
+      echoSuccess "Done"
+    fi
+  fi
+}
+
+donezo() {
+  echo #EMPTY_LINE
+  echoSuccess "----------------------------------"
+  echo #EMPTY_LINE
+  echoSuccess " Install complete!"
+  echo #EMPTY_LINE
+  echoSuccess " Open a new terminal window or"
+  echoSuccess " source your shell config file to"
+  echoSuccess " start using Docker right away."
+  echo #EMPTY_LINE
+  echoSuccess "----------------------------------"
+  echo #EMPTY_LINE
+}
+
+# Begin execution
+hello $1
+checkForExec brew || installBrew
+safeInstall node
+checkForExec lab || installNPMPackage devlab
+safeInstall docker
+safeInstall docker-machine
+safeInstall xhyve
+safeInstall docker-machine-driver-xhyve
+safeInstall docker-machine-nfs
+safeInstall tmux
+checkSetuid docker-machine-driver-xhyve || setSetuid docker-machine-driver-xhyve
+checkMachineExists $MACHINE_NAME || createMachine $MACHINE_NAME
+checkMachineRunning $MACHINE_NAME || startMachine $MACHINE_NAME
+installNFS $MACHINE_NAME
+installEventBridge $MACHINE_NAME
+writeLaunchAgent $MACHINE_NAME
+writeShellConfig $MACHINE_NAME
+donezo
+
